d8be2361ea7f4f8ba201d0bc0fb32ec6bd2666fd
sabari245 <sabari2004@hotmail.com>
2024-04-11 20:29:58 +0530

    added the pricing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Counter from '@/components/sections/counter'
 import Grants from '@/components/sections/grants'
 import Schemes from '@/components/sections/schemes'
 import Features from '@/components/sections/features'
+import Pricing from '@/components/sections/pricing'
 
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
             <Features />
             <Schemes />
             <Grants />
+            <Pricing />
         </main>
     )
 }
