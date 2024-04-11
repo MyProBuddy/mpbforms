71c93d4a81a0941342edaf3904e69344d91acf52
sabari245 <sabari2004@hotmail.com>
2024-04-11 19:02:23 +0530

    added the feature section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Hero from "@/components/sections/hero";
-import Navbar from "@/components/sections/navbar";
-import Image from "next/image";
-import Counter from "@/components/sections/counter";
-import Grants from "@/components/sections/grants";
-import Schemes from "@/components/sections/schemes";
+import Hero from '@/components/sections/hero'
+import Navbar from '@/components/sections/navbar'
+import Image from 'next/image'
+import Counter from '@/components/sections/counter'
+import Grants from '@/components/sections/grants'
+import Schemes from '@/components/sections/schemes'
+import Features from '@/components/sections/features'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-white">
-      <Navbar />
-      <Hero />
-      <Counter />
-      <Schemes />
-      <Grants />
-    </main>
-  );
+    return (
+        <main className='min-h-screen bg-white'>
+            <Navbar />
+            <Hero />
+            <Counter />
+            <Features />
+            <Schemes />
+            <Grants />
+        </main>
+    )
 }
---
+++ components/sections/features.tsx
@@ -0,0 +1,15 @@
+import featureImg from '@/static/features.png'
+import Image from 'next/image'
+
+export default function Features() {
+    return (
+        <section className='flex flex-col items-center gap-8 py-10'>
+            <h1 className='text-[#5F248E] text-[26px] font-semibold uppercase'>
+                Here's what we do
+            </h1>
+            <div>
+                <Image alt='features-img' src={featureImg} />
+            </div>
+        </section>
+    )
+}
