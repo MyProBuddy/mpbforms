e986a5a22118cf4f3b11dcc394809fd0bfb66ac6
sabari245 <sabari2004@hotmail.com>
2024-04-14 16:28:51 +0530

    added wati

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,71 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "MyProBuddy",
-  description: "Know the best Grants & Government Schemes for your business idea / startup",
-};
+    title: 'MyProBuddy',
+    description:
+        'Know the best Grants & Government Schemes for your business idea / startup',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={inter.className}>
+                {children}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                          var url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?36669';
+                          var options = {
+                            "enabled": true,
+                            "chatButtonSetting": {
+                              "backgroundColor": "#00e785",
+                              "ctaText": "Chat with us",
+                              "borderRadius": "25",
+                              "marginLeft": "0",
+                              "marginRight": "20",
+                              "marginBottom": "20",
+                              "ctaIconWATI": false,
+                              "position": "right"
+                            },
+                            "brandSetting": {
+                              "brandName": "MyProBuddy",
+                              "brandSubTitle": "undefined",
+                              "brandImg": "https://i.ibb.co/Tm1NRFQ/15.png",
+                              "welcomeText": "Hi there!\\nHow can we help you?",
+                              "messageText": "Hello, %0A I have a question about Grants & Schemes",
+                              "backgroundColor": "#00e785",
+                              "ctaText": "Chat with us",
+                              "borderRadius": "25",
+                              "autoShow": false,
+                              "phoneNumber": "919952237700"
+                            }
+                          };
+
+                          function loadWidget() {
+                            var script = document.createElement('script');
+                            script.type = 'text/javascript';
+                            script.async = true;
+                            script.src = url;
+                            script.onload = function() {
+                              if (window.CreateWhatsappChatWidget) {
+                                CreateWhatsappChatWidget(options);
+                              }
+                            };
+                            document.body.appendChild(script);
+                          }
+
+                          loadWidget();
+                        `,
+                    }}
+                />
+            </body>
+        </html>
+    )
 }
