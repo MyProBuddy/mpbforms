2d68d6ce1eadd723e7de8ffa9e86fa5fd256bcb7
RahulDev2004 <talktorahul.b@gmail.com>
2024-04-18 02:20:30 +0530

    commit

--- components/sections/heroform.tsx
+++ components/sections/heroform.tsx
@@ -15,8 +15,8 @@ const VismeContactForm: React.FC = () => {
   }, []);
 
 return (
-    <div className='h-full w-screen my-14 ' id="hero">
-            <h1 className='text-2xl text-center font-bold lg:text-[28px] sm:text-3xl text-[#5F248E] mx-2 uppercase'>Know the best <span className='text-[#FF8F40]'>Grants and Schemes </span> for your business idea / startup </h1>
+    <div className='h-full w-screen mt-10 mb-6' id="hero">
+            <h1 className='text-xl text-center font-bold lg:text-[28px] w-full sm:w-7/10 sm:text-3xl text-[#5F248E] px-2 sm:px-4 uppercase'>Know the best <span className='text-[#FF8F40]'>Grants and Government Schemes </span> for your<br className='hidden sm:block'/> business idea / startup </h1>
             <div className="visme_d" data-title="Contact Me Contact Form" data-url="mxgm06x8-contact-me-contact-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="57414"></div>
     </div>
 );
