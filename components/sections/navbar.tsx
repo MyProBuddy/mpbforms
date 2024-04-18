ead0692ed0f6d837a1e039e76c537f8434349563
RahulDev2004 <talktorahul.b@gmail.com>
2024-04-18 23:41:52 +0530

    commit

--- components/sections/navbar.tsx
+++ components/sections/navbar.tsx
@@ -23,9 +23,9 @@ export default function Navbar() {
                 />
             </div>
             <div className='hidden md:flex items-center justify-center gap-14 text-[#ED4935] font-semibold'>
-                <Link href={'/#'}>Home</Link>
-                <Link href={'https://www.linkedin.com/groups/14304535/'}>Join The Community</Link>
-                <Link href={'https://www.linkedin.com/in/amrish-krishnan/'}>View Our Expert Profile</Link>
+                <a target="_blank" href={'/#'}>Home</a>
+                <a target="_blank" href={'https://www.linkedin.com/groups/14304535/'}>Join The Community</a>
+                <a target="_blank" href={'https://www.linkedin.com/in/amrish-krishnan/'}>View Our Expert Profile</a>
             </div>
             <div className='hidden md:block'></div>
             <div className='flex items-center z-20 px-2 md:hidden '>
